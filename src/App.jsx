5f12e8224aa3d993949e2195e0e0b38acb3cd228
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-06-21 13:42:47 +0530

    React useEffect Hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import './App.css'
-import {useEffect, useState} from "react";
-
-const Card = ({title}) => {
-    const [count, setCount] = useState(0);
-    
-    const [hasLiked, setHasLiked] = useState(false);
-
-    useEffect(() => {
-        console.log(`${title} has been liked ${hasLiked}`);
-    }, [hasLiked, title]);
-
-    return (
-        <div className="card" onClick={() => setCount(count + 1)}>
-            <h2>{title} <br/> {count || null}</h2>
-
-            <button
-                onClick={() => setHasLiked(!hasLiked)}
-                style={{color: hasLiked ? '#ff0000' : '#ffffff'}}
-                aria-label={hasLiked ? 'Unlike' : 'Like'}>
-                ♥
-            </button>
-        </div>
-    )
-}
+import React from 'react'
 
 const App = () => {
     return (
-        <div className="card-container">
-            <Card title="Star Wars" rating={5} isCool={true} />
-            <Card title="Avatar" />
-            <Card title="The Lion King" />
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     )
 }
